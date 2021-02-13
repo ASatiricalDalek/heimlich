9f850aea37115abf367e1a70bb9e6a4c0ca9afdd
Connor <dalek124@gmail.com>
2021-02-12 23:45:48 -0500

    Added further comments

--- heimlich.ps1
+++ heimlich.ps1
@@ -1,3 +1,11 @@
+# HEIMLICH - Up-chuck your your Valhiem world into a safe repository
+# By Connor McNamara - 2/12/2021
+
+# Best used in conjunction with Task Scheduler for automated backups
+# https://blog.netwrix.com/2018/07/03/how-to-automate-powershell-scripts-with-task-scheduler/ 
+
+###########################################
+
 # Replace this string with the location of your root Valheim folder (you likely just need to change the username)
 $path = "C:\Users\Connor\AppData\LocalLow\IronGate\Valheim"
 # This sets the backup location
